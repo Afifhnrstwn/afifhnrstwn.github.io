c7a6a228aa82fa00b14e3dcf8e5ffc7d7dbe8055
Afifhnrstwn <afifhnristwn@gmail.com>
2024-09-21 08:54:35 +0700

    Update shopping cart

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.querySelector('#menu').onclick = () => {
 
 // menghilangkan nav samping
 const hamburger = document.querySelector('#menu');
+const searchBtn = document.querySelector('#search-button')
 
 document.addEventListener('click', function(e) {
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -18,6 +19,32 @@ document.addEventListener('click', function(e) {
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclick = () => {
+document.querySelector('#search-button').onclick = (e) => {
     searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
 };
+
+
+// Toggle class active shooping cart
+const ShoopingCard = document.querySelector('.shooping-cart');
+const sc = document.querySelector('#shopping-cart-button')
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    ShoopingCard.classList.toggle('active');
+    e.preventDefault();
+}
+
+// Keluar class acive
+document.addEventListener('click', function(e) {
+    if(!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active')
+    }
+    
+    if(!sc.contains(e.target) && !ShoopingCard.contains(e.target)) {
+        ShoopingCard.classList.remove('active')
+    }
+})
+
+// modal
+const itemDetailModal = document.querySelector('#item-detail-modal')
